2fde345f02fda0a9f4c571db3a5f5f34d255cba2
João <79210049+JoaoLua@users.noreply.github.com>
2024-11-02 16:37:34 -0300

    Add files via upload

---
+++ src/test/java/br/com/arq/cesta_de_compras/CestaDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arq.cesta_de_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CestaDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
