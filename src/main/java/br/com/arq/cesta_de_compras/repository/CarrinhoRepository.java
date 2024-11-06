6066c5ed1266f8702dde7069f70a82ee6d944c88
João <79210049+JoaoLua@users.noreply.github.com>
2024-11-06 17:06:27 -0300

    Delete src directory

--- src/main/java/br/com/arq/cesta_de_compras/repository/CarrinhoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.arq.cesta_de_compras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.arq.cesta_de_compras.entity.Carrinho;
-
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer> {
-    Carrinho findByClienteId(Integer clienteId);
-}
--- src/main/java/br/com/arq/cesta_de_compras/repository/ItemCarrinhoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.arq.cesta_de_compras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.arq.cesta_de_compras.entity.ItemCarrinho;
-
-public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Integer> {
-
-}
